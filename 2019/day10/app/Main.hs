ca869cb8d4f695436ceb35e3fb547afcc89b0fdb
Boarders <callan.mcgill@gmail.com>
2019-12-22 18:15:17 -0500

    Add part one solution for day 10

---
+++ 2019/day10/app/Main.hs
@@ -0,0 +1,151 @@
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+module Main where
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString  as ByteString hiding (unpack)
+import qualified Data.ByteString.Char8 as ByteString
+import Data.Set (Set)
+import qualified Data.Set  as Set
+import Data.Map (Map)
+import qualified Data.Map  as Map
+import Data.Key (foldMapWithKey)
+import Data.Foldable
+import Data.List.Extra (maximumOn, groupOn)
+import Control.Parallel.Strategies
+import Data.List (nub)
+
+import Debug.Trace
+
+main :: IO ()
+main =
+  do
+    inputText <- ByteString.readFile "input.txt"
+    let asteroids = parseInput inputText
+    let testIn = parseInput test
+    let puzzle1Sol = puzzle1 asteroids
+    let point1 = fst puzzle1Sol
+    putStrLn "puzzle1 :"
+    putStrLn $ (replicate 25 ' ') <> (show puzzle1Sol)
+--    putStrLn ""
+--    putStrLn "puzzle2 :"
+--    putStrLn ""
+--    putStrLn $ (replicate 25 ' ') <> (show . puzzle2 $ undefined)
+
+
+puzzle1 :: [Point] -> (Point, Int)
+puzzle1 =  maximumOn snd . asteroidCount
+
+puzzle2 :: Set Point -> String
+puzzle2 = error "to do"
+
+test :: ByteString
+test =
+  "###\n.#.\n..#"
+  
+recentre :: Point -> [Point] -> [Point]
+recentre p = fmap (\q -> q - p)
+
+asteroidsDestroyed :: Point -> [Point] -> Int -> Point
+asteroidsDestroyed center points nth = undefined
+  where
+    newPoints = recentre center points
+    
+    
+    
+
+-- |
+-- This function takes a point p and another point v
+-- and returns True iff the first point lies on the line 
+liesOnLine :: Point -> Point -> Bool
+liesOnLine p1 p2 = p1 .||. p2
+
+
+asteroidCount :: [Point] -> [(Point, Int)]
+asteroidCount points = parMap rpar (\p -> asteroidsInSight p points) points
+
+
+tr :: (Show a) => String -> a -> a
+tr s x = trace (s <> (show x)) x
+
+
+asteroidsInSight :: Point -> [Point] -> (Point, Int)
+asteroidsInSight focus asteroids =
+    (focus, length $ groupFn asteroids)
+  where
+    groupFn :: [Point] -> Set Line
+    groupFn = foldMap (Set.singleton . pointToLine . (subtract focus)) . filter (/= focus)
+
+
+parseInput :: ByteString -> [Point]
+parseInput =
+      foldMapWithKey f
+    . ByteString.lines
+  where
+    f :: Int -> ByteString -> [Point]
+    f y bs =
+      let
+        chars :: [Char]
+        chars = ByteString.unpack bs
+      in
+        foldMapWithKey (h y) chars
+
+    h :: Int -> Int -> Char -> [Point]
+    h y x = \case
+       '#' -> [(Point x y)]
+       _   -> mempty
+
+
+pointToLine :: Point -> Line
+pointToLine (Point 0 0) = error "Can't construct a line from the zero point!"
+pointToLine (Point x y) =
+  let
+    d = gcd x y
+  in
+    Line (x `div` d) (y `div` d)
+
+
+
+data Line = Line
+  { lx :: {-# UNPACK #-} !Int
+  , ly :: {-# UNPACK #-} !Int
+  }
+  deriving (Show, Eq, Ord)
+
+
+
+
+data Point = Point
+  { x :: {-# UNPACK #-} !Int
+  , y :: {-# UNPACK #-} !Int
+  }
+  deriving (Eq)
+
+instance Num Point where
+  (+) (Point x1 y1) (Point x2 y2) = Point (x1 + x2) (y1 + y2)
+  (-) (Point x1 y1) (Point x2 y2) = Point (x1 - x2) (y1 - y2)
+  negate (Point x y) = (Point (- x) (- y))
+
+
+(.||.) :: Point -> Point -> Bool
+(Point 0 0) .||. _  = True
+_ .||. (Point 0 0)  = True
+(Point x1 y1) .||. (Point x2 y2) =
+  let
+    d1 = gcd x1 y1
+    d2 = gcd x2 y2
+    p1 = (Point (x1 `div` d1) (y1 `div` d1))
+    p2 = (Point (x2 `div` d2) (y2 `div` d2))
+  in
+    p1 == p2
+ 
+    
+
+instance Ord Point where
+  (Point x0 y0) <= (Point x1 y1)
+    = y0 <= y1 || (y0 == y1 && x0 <= x1)
+
+instance Show Point where
+  show (Point x y) = fold ["<x=", show x, ",y=", show y, ">"]
