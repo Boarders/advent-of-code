fccb8f09d2d1bfbaf47ffa2651500ed70031246e
Boarders <callan.mcgill@gmail.com>
2020-11-28 16:47:56 -0500

    Fix up Day1

--- 2015/src/Day1.hs
+++ 2015/src/Day1.hs
@@ -1,27 +1,21 @@
+{-# language BangPatterns #-}
+{-# language MagicHash  #-}
+{-# language UnboxedTuples  #-}
 module Day1 where
 
 import qualified Data.ByteString as ByteString
-import Data.ByteString (ByteString)
 import Data.Word
 import Data.Primitive.ByteArray
 import Common
 import Data.Primitive.Types
+import Data.ByteString.Internal
 
 day1 :: IO ()
 day1 = do
   ba <- readFileBA "input/day1.dat"
   let sol1 = countBracketsBA ba
   let sol2 = firstNeg ba
-  putStrLn $ unlines (solutions sol1 sol2)
-
-
-solutions :: Int -> Int -> [String]
-solutions s1 s2 =
-  ["~~~ Day 1 ~~~"
-  ,""
-  ,"solution 1: " <> (show s1)
-  ,"solution 2: " <> (show s2)
-  ]
+  putStrLn $ unlines (solutions 1 sol1 sol2)
 
 countBrackets :: ByteString -> Int
 countBrackets = ByteString.foldl' addBS 0
@@ -31,9 +25,21 @@ countBrackets = ByteString.foldl' addBS 0
     addBS acc 41 = acc - 1
     addBS acc _  = acc
 
+countBracketsBS :: ByteString -> Int
+countBracketsBS bs = myfoldl' addBS 0 bs
+  where
+    addBS :: Int -> Word8 -> Int
+    addBS acc 40 = acc + 1
+    addBS acc 41 = acc - 1
+    addBS acc _  = acc
 
 countBracketsBA :: ByteArray -> Int
-countBracketsBA = foldlBA add 0
+countBracketsBA = foldlBA addBA 0
+  where
+    addBA :: Int -> Word8 -> Int
+    addBA acc 40 = acc + 1
+    addBA acc 41 = acc - 1
+    addBA acc _  = acc
 
 add :: Int -> Word8 -> Int
 add acc 40 = acc + 1
@@ -49,5 +55,3 @@ firstNeg arr = go 0 0
       | i < maxI  = go (i + 1) (add acc (indexByteArray arr i)) 
       | otherwise = error "Not found"
     maxI = sizeofByteArray arr `quot` sizeOf (undefined :: Word8)
-
-
