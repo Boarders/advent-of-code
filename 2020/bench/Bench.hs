943b9d9607decaa7fe86d265e6b7f486297c289a
Boarders <callan.mcgill@gmail.com>
2020-12-01 13:25:53 -0500

    Fix names

--- 2020/bench/Bench.hs
+++ 2020/bench/Bench.hs
@@ -2,7 +2,7 @@ module Main where
 
 import qualified Criterion.Main                             as C (bench, bgroup,
                                                                   defaultMain,
-                                                                  env, nf)
+                                                                  env, nf, nfIO)
 import qualified Data.ByteString                            as ByteString
 import Day1
 
@@ -18,8 +18,9 @@ main = do
   C.defaultMain . pure $
     C.env getInput $ \ ~is ->
       C.bgroup "day1"
-        [ C.bench "naive"   $ C.nf sum_2020 is
-        , C.bench "intSet"  $ C.nf s1 is
-        , C.bench "naive2"  $ C.nf sum3_2020 is
-        , C.bench "intSet2" $ C.nf s2 is
+        [ C.bench "naive"    $ C.nf sum_2020 is
+        , C.bench "intSet"   $ C.nf s1 is
+        , C.bench "naive2"   $ C.nf sum3_2020 is
+        , C.bench "intSet2"  $ C.nf s2 is
+        , C.bench "solution" $ C.nfIO day1
         ]
--- 2020/src/Day1.hs
+++ 2020/src/Day1.hs
@@ -11,9 +11,18 @@ day1 = do
   bs <- ByteString.readFile "input/day1.dat"
   let
     input = parseInput bs
-    sol1 = sum_2020 input
-    sol2 = sum3_2020 input
+    sol1 = s1 input
+    sol2 = s2 input
   putStrLn $ (solutions 1 sol1 sol2)
+
+day1' :: IO (Int, Int)
+day1' = do
+  bs <- ByteString.readFile "input/day1.dat"
+  let
+    input = parseInput bs
+    sol1 = s1 input
+    sol2 = s2 input
+  pure (sol1, sol2)
 
 parseInput :: ByteString -> [Int]
 parseInput = fmap (fst . fromJust . ByteString.readInt) . ByteString.lines
@@ -27,13 +36,13 @@ sum3_2020 xs = head [ x * y * z | x <- xs, y <- tail xs, z <- tail (tail xs), x 
 
 
 s1 :: [Int] -> Int
-s1 xs = head [ x * (2020 - x) | x <- xs, (2020 - x) `IntSet.member` hs]
+s1 xs = head [ x * (2020 - x) | x <- xs, (2020 - x) `IntSet.member` ints]
   where
-    hs :: IntSet.IntSet
-    hs = IntSet.fromList xs
+    ints :: IntSet.IntSet
+    ints = IntSet.fromList xs
 
 s2 :: [Int] -> Int
-s2 xs = head [ x * y * (2020 - x - y) | x <- xs, y <- tail xs, (2020 - x - y) `IntSet.member` hs]
+s2 xs = head [ x * y * (2020 - x - y) | x <- xs, y <- tail xs, (2020 - x - y) `IntSet.member` ints]
   where
-    hs :: IntSet.IntSet
-    hs = IntSet.fromList xs
+    ints :: IntSet.IntSet
+    ints = IntSet.fromList xs
