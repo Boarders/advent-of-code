d8e9eed3eddf6c9aa2514578f1b99d7c9e8d960c
Boarders <callan.mcgill@gmail.com>
2020-12-01 12:41:17 -0500

    Add more efficient version

--- 2020/src/Day1.hs
+++ 2020/src/Day1.hs
@@ -1,16 +1,10 @@
-{-# language ScopedTypeVariables #-}
-{-# language BangPatterns #-}
-{-# language DeriveGeneric #-}
-{-# language LambdaCase #-}
-{-# language OverloadedStrings #-}
-{-# language DuplicateRecordFields #-}
-{-# language PatternSynonyms #-}
 module Day1 where
 
 import qualified Data.ByteString.Char8 as ByteString
 import Data.ByteString.Char8 (ByteString)
 import Common (solutions)
 import Data.Maybe (fromJust)
+import qualified Data.IntSet as IntSet
 
 day1 :: IO ()
 day1 = do
@@ -24,10 +18,22 @@ day1 = do
 parseInput :: ByteString -> [Int]
 parseInput = fmap (fst . fromJust . ByteString.readInt) . ByteString.lines
 
-
 sum_2020 :: [Int] -> Int
 sum_2020 xs = head [ x * y | x <- xs, y <- tail xs, x + y == 2020]
   
 
 sum3_2020 :: [Int] -> Int
 sum3_2020 xs = head [ x * y * z | x <- xs, y <- tail xs, z <- tail (tail xs), x + y + z == 2020]
+
+
+s1 :: [Int] -> Int
+s1 xs = head [ x * (2020 - x) | x <- xs, (2020 - x) `IntSet.member` hs]
+  where
+    hs :: IntSet.IntSet
+    hs = IntSet.fromList xs
+
+s2 :: [Int] -> Int
+s2 xs = head [ x * y * (2020 - x - y) | x <- xs, y <- tail xs, (2020 - x - y) `IntSet.member` hs]
+  where
+    hs :: IntSet.IntSet
+    hs = IntSet.fromList xs
