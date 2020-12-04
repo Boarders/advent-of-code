06ab5b55ac88f84b765803a10a72630589783f3e
Boarders <callan.mcgill@gmail.com>
2020-12-04 18:37:12 -0500

    Use fully qualified name

--- 2020/bench/Bench.hs
+++ 2020/bench/Bench.hs
@@ -1,9 +1,8 @@
 {-# language BlockArguments #-}
 module Main where
 
-import qualified Criterion.Main                             as C (bench, bgroup,
-                                                                  defaultMain,
-                                                                  env, nf)
+import qualified Criterion.Main                             as Criterion
+  (bench, bgroup, defaultMain, env, nf)
 import qualified Day1 as Day1
 import qualified Day2 as Day2
 import qualified Day3 as Day3
@@ -11,21 +10,21 @@ import qualified Day3 as Day3
 
 main :: IO ()
 main = do
-  C.defaultMain . pure $
-    C.bgroup "advent of code"
-    [ C.env Day1.parseInput $ \ ~d1 ->
-         C.bgroup "day1"
-        [ C.bench "sol1"   $ C.nf Day1.s1 d1
-        , C.bench "sol2"  $ C.nf Day1.s2 d1
+  Criterion.defaultMain . pure $
+    Criterion.bgroup "advent of code"
+    [ Criterion.env Day1.parseInput $ \ ~d1 ->
+         Criterion.bgroup "day1"
+        [ Criterion.bench "sol1"   $ Criterion.nf Day1.s1 d1
+        , Criterion.bench "sol2"  $ Criterion.nf Day1.s2 d1
         ]
-    , C.env Day2.parseInput $ \ ~d2 ->
-        C.bgroup "day2"
-       [ C.bench "sol1"    $ C.nf Day2.s1 d2
-       , C.bench "sol2"    $ C.nf Day2.s2 d2
+    , Criterion.env Day2.parseInput $ \ ~d2 ->
+        Criterion.bgroup "day2"
+       [ Criterion.bench "sol1"    $ Criterion.nf Day2.s1 d2
+       , Criterion.bench "sol2"    $ Criterion.nf Day2.s2 d2
        ]
-    , C.env Day3.parseInput $ \ ~d3 ->
-        C.bgroup "day3"
-       [ C.bench "sol1"    $ C.nf Day3.s1 d3
-       , C.bench "sol2"    $ C.nf Day3.s2 d3
+    , Criterion.env Day3.parseInput $ \ ~d3 ->
+        Criterion.bgroup "day3"
+       [ Criterion.bench "sol1"    $ Criterion.nf Day3.s1 d3
+       , Criterion.bench "sol2"    $ Criterion.nf Day3.s2 d3
        ]       
     ]
