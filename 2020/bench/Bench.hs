c3e6c85f4d7b9c71d9e5cded4453c22f63070db8
Boarders <callan.mcgill@gmail.com>
2020-12-05 16:10:46 -0500

    Move benchmark around so it is easier to work with

--- 2020/bench/Bench.hs
+++ 2020/bench/Bench.hs
@@ -1,36 +1,53 @@
 {-# language BlockArguments #-}
 module Main where
 
-import qualified Criterion.Main                             as Criterion
-  (bench, bgroup, defaultMain, env, nf)
+import qualified Criterion.Main as Criterion
 import qualified Day1 as Day1
 import qualified Day2 as Day2
 import qualified Day3 as Day3
 import qualified Day4 as Day4
+import qualified Day5 as Day5
+
+
+day1, day2, day3, day4, day5 :: Criterion.Benchmark
+day1 = Criterion.env Day1.parseInput $ \ ~d ->
+  Criterion.bgroup "day1"
+    [ Criterion.bench "sol1"   $ Criterion.nf Day1.s1 d
+    , Criterion.bench "sol2"  $ Criterion.nf Day1.s2 d
+    ]
+
+day2 = Criterion.env Day2.parseInput $ \ ~d ->
+  Criterion.bgroup "day2"
+    [ Criterion.bench "sol1"    $ Criterion.nf Day2.s1 d
+    , Criterion.bench "sol2"    $ Criterion.nf Day2.s2 d
+    ]
+
+day3 = Criterion.env Day3.parseInput $ \ ~d ->
+  Criterion.bgroup "day3"
+    [ Criterion.bench "sol1"    $ Criterion.nf Day3.s1 d
+    , Criterion.bench "sol2"    $ Criterion.nf Day3.s2 d
+    ]
+
+day4 = Criterion.env Day4.parseInput $ \ ~d ->
+  Criterion.bgroup "day4"
+    [ Criterion.bench "sol1"    $ Criterion.nf Day4.s1 d
+    , Criterion.bench "sol2"    $ Criterion.nf Day4.s2 d
+    ]
+
+day5 = Criterion.env Day5.parseInput $ \ ~d ->
+  Criterion.bgroup "day5"
+    [ Criterion.bench "sol1"    $ Criterion.nf Day5.s1 d
+    , Criterion.bench "sol2"    $ Criterion.nf Day5.s2 d
+    ]
 
 main :: IO ()
 main = do
   Criterion.defaultMain . pure $
     Criterion.bgroup "advent of code"
-    [
-      Criterion.env Day1.parseInput $ \ ~d1 ->
-         Criterion.bgroup "day1"
-        [ Criterion.bench "sol1"   $ Criterion.nf Day1.s1 d1
-        , Criterion.bench "sol2"  $ Criterion.nf Day1.s2 d1
-        ]
-    , Criterion.env Day2.parseInput $ \ ~d2 ->
-        Criterion.bgroup "day2"
-       [ Criterion.bench "sol1"    $ Criterion.nf Day2.s1 d2
-       , Criterion.bench "sol2"    $ Criterion.nf Day2.s2 d2
-       ]
-    , Criterion.env Day3.parseInput $ \ ~d3 ->
-        Criterion.bgroup "day3"
-       [ Criterion.bench "sol1"    $ Criterion.nf Day3.s1 d3
-       , Criterion.bench "sol2"    $ Criterion.nf Day3.s2 d3
-       ]
-    , Criterion.env Day4.parseInput $ \ ~d4 ->
-        Criterion.bgroup "day4"
-       [ Criterion.bench "sol1"    $ Criterion.nf Day4.s1 d4
-       , Criterion.bench "sol2"    $ Criterion.nf Day4.s2 d4
-       ]
+    [ day5
+--      day1
+--    , day2
+--    , day3
+--    , day4
+--    , day5
     ]
