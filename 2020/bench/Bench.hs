d09b5d95e79c033bcc4b6930ee40757b35083d23
Boarders <callan.mcgill@gmail.com>
2020-12-10 16:15:57 -0500

    Add day 10

--- 2020/bench/Bench.hs
+++ 2020/bench/Bench.hs
@@ -11,29 +11,30 @@ import qualified Day6 as Day6
 import qualified Day7 as Day7
 import qualified Day8 as Day8
 import qualified Day9 as Day9
+import qualified Day10 as Day10
 
 
 
 
-day1, day2, day3, day4, day5, day6 :: Criterion.Benchmark
+day1, day2, day3, day4, day5, day6, day7, day8, day9, day10 :: Criterion.Benchmark
 day1 = Criterion.env Day1.parseInput $ \ ~d ->
   Criterion.bgroup "day1"
-    [ Criterion.bench "sol1"   $ Criterion.nf Day1.s1 d
-    , Criterion.bench "sol2"  $ Criterion.nf Day1.s2 d
+    [ Criterion.bench "sol1" $ Criterion.nf Day1.s1 d
+    , Criterion.bench "sol2" $ Criterion.nf Day1.s2 d
     , Criterion.bench "tot"  $ Criterion.nfIO Day1.day1'
     ]
 
 day2 = Criterion.env Day2.parseInput $ \ ~d ->
   Criterion.bgroup "day2"
-    [ Criterion.bench "sol1"    $ Criterion.nf Day2.s1 d
-    , Criterion.bench "sol2"    $ Criterion.nf Day2.s2 d
+    [ Criterion.bench "sol1" $ Criterion.nf Day2.s1 d
+    , Criterion.bench "sol2" $ Criterion.nf Day2.s2 d
     , Criterion.bench "tot"  $ Criterion.nfIO Day2.day2'
     ]
 
 day3 = Criterion.env Day3.parseInput $ \ ~d ->
   Criterion.bgroup "day3"
-    [ Criterion.bench "sol1"    $ Criterion.nf Day3.s1 d
-    , Criterion.bench "sol2"    $ Criterion.nf Day3.s2 d
+    [ Criterion.bench "sol1" $ Criterion.nf Day3.s1 d
+    , Criterion.bench "sol2" $ Criterion.nf Day3.s2 d
     , Criterion.bench "tot"  $ Criterion.nfIO Day3.day3'
     ]
 
@@ -69,30 +70,33 @@ day7 = Criterion.env Day7.parseInput $ \ ~d ->
 day8 = Criterion.env Day8.parseInput $ \ d ->
   Criterion.bgroup "day8"
     [
-      Criterion.bench "sol1"    $ Criterion.nf Day8.s1 d
-    , Criterion.bench "sol2"    $ Criterion.nf Day8.s2 d
-    , Criterion.bench "tot"    $ Criterion.nfIO Day8.day8'
+      Criterion.bench "sol1" $ Criterion.nf Day8.s1 d
+    , Criterion.bench "sol2" $ Criterion.nf Day8.s2 d
+    , Criterion.bench "tot"  $ Criterion.nfIO Day8.day8'
     ]
 
 day9 = Criterion.env Day9.parseInput $ \ d ->
   Criterion.bgroup "day9"
     [
-      Criterion.bench "sol1"    $ Criterion.nf Day9.s1 d
-    , Criterion.bench "sol2"    $ Criterion.nf (Day9.s2 257342611) d
-    , Criterion.bench "tot"    $ Criterion.nfIO Day9.day9'
+      Criterion.bench "sol1" $ Criterion.nf Day9.s1 d
+    , Criterion.bench "sol2" $ Criterion.nf (Day9.s2 257342611) d
+    , Criterion.bench "tot"  $ Criterion.nfIO Day9.day9'
     
-    ]    
+    ]
 
+day10 = Criterion.env Day10.parseInput $ \ d ->
+  Criterion.bgroup "day10"
+    [
+      Criterion.bench "sol1" $ Criterion.nf Day10.s1 d
+    , Criterion.bench "sol2" $ Criterion.nf Day10.s2 d
+    , Criterion.bench "tot"  $ Criterion.nfIO Day10.day10'
+    ]        
+
+days :: [Criterion.Benchmark]
 days = [day1, day2, day3, day4, day5, day6, day7,day8, day9]
 main :: IO ()
 main = do
   Criterion.defaultMain . pure $
     Criterion.bgroup "advent of code"
-    days
---    [ day9
---      day1
---    , day2
---    , day3
---    , day4
---    , day5
---    ]
+    [ day10 ]
+
---
+++ 2020/src/Day10.hs
@@ -0,0 +1,161 @@
+{-# language ScopedTypeVariables #-}
+{-# language OverloadedStrings #-}
+
+module Day10 where
+
+import qualified Data.ByteString.Char8 as ByteString
+import Common (solutions, bsToInt)
+import Data.Vector.Unboxed (Vector)
+import qualified Data.Vector.Unboxed as Vector
+import Data.Vector.Unboxed.Mutable (MVector)
+import Data.Vector.Algorithms.Radix (sort)
+import Control.Monad.ST
+
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+
+
+day10 :: IO ()
+day10 = do
+  inp <- parseInput
+  let
+    sol1 = s1 inp
+    sol2 = s2 inp
+  solutions 10 sol1 sol2
+
+day10' :: IO (Int, Int)
+day10' = do
+  inp <- parseInput
+  let
+    sol1 = s1 inp
+    sol2 = s2 inp
+  pure (sol1, sol2)
+
+
+parseInput :: IO (Vector Int)
+parseInput = do
+  bs <- ByteString.readFile "input/day10.dat"
+  let vs = Vector.fromList . fmap bsToInt . ByteString.lines $ bs
+  pure $ Vector.create $ do
+    mv <- Vector.unsafeThaw vs
+    sort mv
+    pure mv
+
+sortInPlace :: forall s . Vector Int -> ST s (MVector s Int)
+sortInPlace vs = do
+  mv <- Vector.unsafeThaw vs
+  sort mv
+  pure mv
+
+data State = State
+  { lastEntry :: !Int
+  , oneJolt   :: !Int
+  , threeJolt :: !Int 
+  }
+  deriving Show
+
+multJolt :: State -> Int
+multJolt (State _ oneJ threeJ) = oneJ * threeJ
+
+s1 :: Vector Int -> Int
+s1 v =
+  let
+    h  = Vector.head v
+    v' = Vector.tail v
+    start = State h 1 1
+  in
+    multJolt . Vector.foldl' count start $ v'
+  where
+    count :: State -> Int -> State
+    count (State lastEnt oneJ threeJ) n =
+        case n - lastEnt of
+          1 -> State n (oneJ + 1) (threeJ)
+          3 -> State n oneJ       (threeJ + 1)
+          _ -> State n oneJ       threeJ
+
+
+buildGraph :: Vector Int -> IntMap (Vector Int)
+buildGraph v = Vector.ifoldl' addVert startMap v
+  where
+    startMap = IntMap.insert highest mempty mempty
+    highest  = (Vector.last v + 3)
+    len = Vector.length v
+    addVert :: IntMap (Vector Int) -> Int -> Int -> IntMap (Vector Int)
+    addVert acc ind val =
+      let
+        entries =
+            Vector.filter (<= val + 3)
+          . Vector.slice (ind + 1) (min (len - (ind + 1)) 3)
+          $ v
+      in
+        IntMap.insert val entries acc
+
+
+paths :: Vector Int -> IntMap (Vector Int) -> Int
+paths v g =
+      Vector.foldl' (\acc x -> acc + (intMap IntMap.! x)) 0
+    . Vector.filter (<= 3)
+    . Vector.take 3
+    $ v
+  where
+    highest = Vector.last v + 3
+    intMap  = IntMap.fromSet i (IntMap.keysSet g)
+    i :: Int -> Int
+    i ind | highest - ind <= 3 = 1
+          | otherwise =
+              Vector.foldl' (\acc x -> acc + (intMap IntMap.! x)) 0 (g IntMap.! ind)
+    
+s2 :: Vector Int -> Int
+s2 v = paths v (buildGraph v)
+
+
+{-
+test = ByteString.unlines $
+  [ "16"
+  , "10"
+  , "15"
+  , "5"
+  , "1"
+  , "11"
+  , "7"
+  , "19"
+  , "6"
+  , "12"
+  , "4"
+  ]
+
+test2 =
+  ByteString.unlines
+  [ "28"
+  , "33"
+  , "18"
+  , "42"
+  , "31"
+  , "14"
+  , "46"
+  , "20"
+  , "48"
+  , "47"
+  , "24"
+  , "23"
+  , "49"
+  , "45"
+  , "19"
+  , "38"
+  , "39"
+  , "11"
+  , "1"
+  , "32"
+  , "25"
+  , "35"
+  , "8"
+  , "17"
+  , "7"
+  , "9"
+  , "4"
+  , "2"
+  , "34"
+  , "10"
+  , "3"
+  ]
+--}
