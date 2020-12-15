4b10fdc73b08d9eb31b93fccb27af496ae9a7ef7
Boarders <callan.mcgill@gmail.com>
2020-12-14 20:27:53 -0500

    Speed up day12 part2

--- 2020/src/Day12.hs
+++ 2020/src/Day12.hs
@@ -10,23 +10,19 @@ module Day12 where
 import qualified Data.ByteString.Char8 as ByteString
 import Data.ByteString.Char8 (ByteString)
 import Common (solutions, bsToInt)
-import Data.Foldable (for_, traverse_)
-import Data.Vector.Unboxed.Deriving (derivingUnbox)
 import Data.List (foldl')
-import Data.Semigroup (Semigroup(stimes))
+import Control.DeepSeq
+import GHC.Generics (Generic)
+
 
 day12 :: IO ()
 day12 = do
   inp <- parseInput
   let
     sol1, sol2 :: Int
-    sol1 = 1 -- s1 inp
-    sol2 = 1 -- s2 inp
---  print inp
---  print $ s1 inp
---  solutions 12 sol1 sol2
-  print $ s2 inp
-
+    sol1 = s1 inp
+    sol2 = s2 inp
+  solutions 12 sol1 sol2
   
 
 day12' :: IO (Int, Int)
@@ -34,15 +30,14 @@ day12' = do
   inp <- parseInput
   let
     sol1, sol2 :: Int
-    sol1 = 1 -- s1 inp
-    sol2 = 1 -- s2 inp
+    sol1 = s1 inp
+    sol2 = s2 inp
   pure (sol1, sol2)
 
 
 parseInput :: IO [Instr]
 parseInput = do
   bs <- ByteString.readFile "input/day12.dat"
---  let bs = test
   let instrs = fmap parseEntry . ByteString.lines $ bs
   pure instrs
 
@@ -62,6 +57,7 @@ parseEntry bs = case ByteString.head bs of
     270 -> TurnInstr L
     n   -> error $ show n
   'F' -> FrwdInstr n
+  _ -> error "parseEntry: unexpected input"
   where
     n = bsToInt (ByteString.tail bs)
 
@@ -71,10 +67,15 @@ s1 = manhattan . pos . runInstr
 s2 :: [Instr] -> Int
 s2 = manhattan . ship . interp2
 
-
 data Dir  = N | E | S | W
-  deriving Show
+  deriving (Show, Generic)
+
+instance NFData Dir
+
 data Turn = L | R | F
+  deriving Generic
+
+instance NFData Turn
 
 instance Show Turn where
   show = \case
@@ -86,6 +87,10 @@ data Move = Move
   { dir :: !Dir
   , len :: !Int
   }
+  deriving Generic
+
+instance NFData Move
+
 instance Show Move where
   show (Move d l) = show d <> show l
 
@@ -93,6 +98,9 @@ data Instr =
     MoveInstr !Move
   | TurnInstr !Turn
   | FrwdInstr !Int
+  deriving (Generic)
+
+instance NFData Instr
 
 instance Show Instr where
   show = \case
@@ -110,32 +118,30 @@ instance Semigroup Point where
   (Point x1 y1) <> (Point x2 y2) = (Point (x1 + x2) (y1 + y2))
 
 
-
 data State = State
-  { pos :: Point
-  , dir :: Dir
+  { pos :: !Point
+  , dir :: !Dir
   }
 
 startState :: State
 startState = State (Point 0 0) E
-  
 
 runInstr :: [Instr] -> State
 runInstr = foldl' interp startState
   where
     interp :: State -> Instr -> State
-    interp (State pos dir) = \case
+    interp !(State pos dir) = \case
       MoveInstr m -> State (movePos pos m)            dir
       TurnInstr t -> State pos                       (changeDir dir t)
       FrwdInstr n -> State (movePos pos (Move dir n)) dir
 
-    movePos (Point x y) (Move d n) =
+    movePos !(Point x y) !(Move d n) =
       case d of
         E -> (Point (x + n) y)
         W -> (Point (x - n) y)
         S -> (Point x       (y - n))
         N -> (Point x       (y + n))
-      
+
 changeDir :: Dir -> Turn -> Dir
 changeDir E =
   \case
@@ -146,36 +152,39 @@ changeDir N =
   \case
     L -> W
     R -> E
-    F -> S  
+    F -> S
 changeDir S =
   \case
     L -> E
     R -> W
-    F -> N    
+    F -> N
 changeDir W =
   \case
     L  -> S
     R  -> N
-    F  -> E    
+    F  -> E
 
 startState2 = State2 (Point 0 0) (Point 10 1)
 
 data State2 = State2
-  { ship     :: Point
-  , waypoint :: Point
+  { ship     :: !Point
+  , waypoint :: !Point
   }
   deriving Show
+
+mulPoint :: Int -> Point -> Point
+mulPoint n (Point x y) = Point (n * x) (n * y)
 
 interp2 :: [Instr] -> State2
 interp2 = foldl' interp startState2
   where
   interp :: State2 -> Instr -> State2
-  interp (State2 ship wayP) = \case
+  interp !(State2 ship wayP) = \case
     MoveInstr m -> State2 ship (movePos wayP m)
     TurnInstr t -> State2 ship (rotate wayP t)
-    FrwdInstr n -> State2 (ship <> (stimes n wayP)) wayP
-    
-  movePos (Point x y) (Move d n) =
+    FrwdInstr n -> State2 (ship <> (mulPoint n wayP)) wayP
+
+  movePos !(Point x y) !(Move d n) =
     case d of
       E -> (Point (x + n) y)
       W -> (Point (x - n) y)
@@ -183,19 +192,10 @@ interp2 = foldl' interp startState2
       N -> (Point x       (y + n))
 
 rotate :: Point -> Turn -> Point
-rotate (Point x y) = \case
+rotate !(Point x y) = \case
   R -> Point y     (- x)
   F -> Point (- x) (- y)
   L -> Point (- y)  x
 
 manhattan :: Point -> Int
 manhattan (Point x y) = abs x + abs y
-
-
-test = ByteString.unlines
-  [ "F10"
-  , "N3"
-  , "F7"
-  , "R90"
-  , "F11"
-  ]
